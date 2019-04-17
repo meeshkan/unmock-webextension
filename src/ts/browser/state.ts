1c454a16199d21b581269652138a396f27f395b7
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-17 13:33:26 +0300

    Write types for storage state (#5)
    * Add spec for state, migrate active.
    
    * Start refactoring labeling.
    
    * Add phase enum for marking what to do next.
    
    * Simplify a bit.

---
+++ src/ts/browser/state.ts
@@ -0,0 +1,49 @@
+export enum Phase {
+  ADD_PATH = "Add path",
+  ADD_OPERATION = "Add operation",
+}
+
+export interface Active {
+  url?: string;
+  /**
+   * Active path in Paths object:
+   * Examples:
+   * []: User needs to select a path such as "/pets/:id" for the currently active URL
+   * ["/pets/:id"]: User needs to select an operation
+   * ["/pets/:id", "get"]: User is filling in an operation, with default value such as "description" appended to the selection if nothing chosen
+   */
+  //
+  activePath?: string[];
+  phase: Phase;
+}
+
+export interface State {
+  /**
+   * Current active state: what is being labeled.
+   */
+  active: Active;
+  /**
+   * What has been labeled.
+   */
+  labeled: Labeled;
+}
+
+export interface Labeled {
+  [url: string]: Paths;
+}
+
+// Analogous to
+// https://swagger.io/specification/#pathsObject
+interface Paths {
+  // For example: "/pets/:id" -> Path
+  [path: string]: Path;
+}
+
+// Analogous to
+// https://swagger.io/specification/#pathItemObject
+interface Path {
+  // For example: "get" -> Operation
+  [operation: string]: Operation;
+}
+
+interface Operation {}
