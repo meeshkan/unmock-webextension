dad6c6e1a1a05b4b3804f3f9ddd4795cc20acb39
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-17 16:17:10 +0300

    Work on explorer...

---
+++ src/ts/explorer/activeStateComponent.tsx
@@ -0,0 +1,30 @@
+import "bootstrap/dist/css/bootstrap.css";
+import * as React from "react";
+import { hot } from "react-hot-loader";
+import { Active } from "../browser/state";
+
+interface Props {
+  active: Active;
+}
+
+class ActiveStateComponent extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  public render() {
+    const { active } = this.props;
+    const { activePath, url, phase } = active;
+    return (
+      <div>
+        <div id="active">
+          <p>Active URL: {url}</p>
+          <p>Active phase: {phase}</p>
+          <p>Active path: {activePath}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(ActiveStateComponent);
