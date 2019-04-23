1b5cc8d4027566cd137802a22dea3996cc5e471f
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-23 09:48:41 +0300

    Add a story with context provider.

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import * as React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from "@storybook/react/demo";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import LabeledUrlsComponent from "../src/ts/explorer/list-item/labeledComponent";
+import { Labeled, State } from "../src/ts/state";
+import { Actions } from "../src/ts/explorer/actions";
+import { ExplorerActionsContext } from "../src/ts/explorer/context";
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+const labeled: Labeled = {};
+
+const actions: Actions = {
+  triggerFetchSuccess(data: State) {},
+  triggerSetActiveUrl(url: string) {},
+};
+
+storiesOf("Labeled", module)
+  .addDecorator(story => (
+    <ExplorerActionsContext.Provider value={{ actions }}>
+      {story()}
+    </ExplorerActionsContext.Provider>
+  ))
+  .add("Labeled URLs", () => <LabeledUrlsComponent labeled={labeled} />);
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
