ac312e47724d0d4ce36960873463ce8b0c81d210
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-24 11:37:44 +0300

    Setup tests (#12)
    * Setup enzyme.
    
    * Add simple test for explorer buttons.
    
    * Clean-up.
    
    * Move enzyme-to-json to dev.
    
    * Use jest-webextension-mock to mock browser.
    
    * Rename test.
    
    * Remove unused stuff.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  // Setup Enzyme
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFiles: ["jest-webextension-mock"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+};
