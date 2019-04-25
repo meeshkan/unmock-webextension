369594b8c54e4a4040f861768f0ceb097d50b0c4
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-25 11:36:42 +0300

    Update mock data for user state component.

--- src/ts/explorer/user-state-component.test.tsx
+++ src/ts/explorer/user-state-component.test.tsx
@@ -1,28 +1,7 @@
 import * as React from "react";
 import UserStateComponent from "./user-state-component";
 import { shallow } from "enzyme";
-
-const mockUserState = {
-  actions: [{ type: "log" }],
-  activities: {},
-  changed: true,
-  context: { path: [] },
-  event: { type: "NEXT" },
-  events: [],
-  history: {
-    actions: [{ type: "log" }],
-    activities: {},
-    context: { path: [] },
-    event: { type: "NEXT" },
-    events: [],
-    historyValue: { current: "addOperation", states: {} },
-    meta: {},
-    value: "addOperation",
-  },
-  historyValue: { current: "addPath", states: {} },
-  meta: {},
-  value: "addPath",
-};
+import { mockUserState } from "./__mocks__";
 
 describe("User state component", () => {
   test("renders correctly", () => {
