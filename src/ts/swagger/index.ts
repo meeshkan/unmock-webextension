4c8948b61f8ba64ade227ae42e4e192a490a1b35
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-26 15:01:59 +0300

    Declare globals.

--- src/ts/swagger/index.ts
+++ src/ts/swagger/index.ts
@@ -1,14 +1,14 @@
 import { MyWrapActionPlugin } from "./wrap-spec-action";
 
+// These are loaded in swagger.html scripts
+declare let SwaggerEditorBundle: any;
+declare let SwaggerEditorStandalonePreset: any;
+
 window.onload = () => {
-  // Build a system
-  // These are loaded in swagger.html scripts
-  // @ts-ignore
   const editor = SwaggerEditorBundle({
     dom_id: "#swagger-editor",
     layout: "StandaloneLayout",
     plugins: [MyWrapActionPlugin],
-    // @ts-ignore
     presets: [SwaggerEditorStandalonePreset],
   });
   (window as any).editor = editor;
