8ce7a1f3f5e99478d649c78d4d6056a7ac14dbff
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-02 14:28:29 +0300

    A few more tests.

--- src/ts/parsers/page.spec.ts
+++ src/ts/parsers/page.spec.ts
@@ -3,13 +3,17 @@ import { PageContent } from "../common/types";
 
 const pageContent: PageContent = {
   title: "Page title",
-  innerHtml: "<html></html><body>GET /v1/pets</body>",
-  textContent: "GET /v1/pets",
+  innerHtml: "<html></html><body>GET /v1/pets PUT /v2/pets</body>",
+  textContent: "GET /v1/pets PUT /v2/pets",
 };
 
 describe("Building spec from page content", () => {
   test("parses paths", async () => {
     const spec = await buildSpecFrom(pageContent);
-    expect(Object.keys(spec.paths)).toHaveLength(1);
+    expect(Object.keys(spec.paths)).toHaveLength(2);
+    const pathItem = spec.paths["/v1/pets"];
+    expect(pathItem).toBeDefined();
+    const operationItem = pathItem.get;
+    expect(operationItem).toBeDefined();
   });
 });
