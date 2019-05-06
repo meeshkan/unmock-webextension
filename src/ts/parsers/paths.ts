9cf9cb0847191eddda8275a6ec48fe93eb61c8c5
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-06 10:12:39 +0300

    Also handle HubSpot-like apis.

--- src/ts/parsers/paths.ts
+++ src/ts/parsers/paths.ts
@@ -1,11 +1,4 @@
-import {
-  OpenApiBuilder,
-  OpenAPIObject,
-  PathsObject,
-  OperationObject,
-  PathItemObject,
-  ParameterObject,
-} from "openapi3-ts";
+import { ParameterObject } from "openapi3-ts";
 import * as _ from "lodash";
 
 export interface Path {
@@ -13,26 +6,52 @@ export interface Path {
   pathParameters: ParameterObject[];
 }
 
+const cleanPartOfPathAndExtractParameters = (
+  str: string
+): [string, ParameterObject | undefined] => {
+  const rtdPathPattern = /^\(([\w_]+):\s?([\w_]+)\)$/;
+  const rtdMatch = str.match(rtdPathPattern);
+  const colonMatch = str.match(/^:([\w_]+)/);
+  if (rtdMatch) {
+    const parameterName = rtdMatch[2];
+    // TODO Better
+    const schema = rtdMatch[1] === "int" ? { type: "integer" } : {};
+    const parameterObject: ParameterObject = {
+      name: parameterName,
+      in: "path",
+      required: true,
+      schema,
+    };
+    return [`:${parameterName}`, parameterObject];
+  } else if (colonMatch) {
+    const parameterName = colonMatch[1];
+    const schema = {};
+    const parameterObject: ParameterObject = {
+      name: parameterName,
+      in: "path",
+      required: true,
+      schema,
+    };
+    return [str, parameterObject];
+  }
+  return [str, null];
+};
+
 export const extractPathParametersRtd = (path: Path): Path => {
   const pathSplit = path.name.split("/");
   const params: ParameterObject[] = [];
   const cleanedPathSplit = pathSplit.map(value => {
-    const rtdPathPattern = /^\((\w+):\s?(\w+)\)$/;
-    const match = value.match(rtdPathPattern);
-    if (match) {
-      // const parameterType = match[0];
-      const parameterName = match[2];
-      // Urgh, ugly side-effect in map
-      const parameterObject: ParameterObject = {
-        name: parameterName,
-        in: "path",
-        required: true,
-        schema: {}, // TODO
-      };
-      params.push(parameterObject);
-      return `:${parameterName}`;
+    const [
+      cleanedPath,
+      maybeParameterObject,
+    ] = cleanPartOfPathAndExtractParameters(value);
+
+    // Urgh, ugly side-effect in map
+    if (maybeParameterObject) {
+      params.push(maybeParameterObject);
     }
-    return value;
+
+    return cleanedPath;
   });
   return {
     ...path,
